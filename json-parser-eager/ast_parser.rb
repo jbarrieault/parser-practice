0d8782110d9ec69b9256ac13daf8b9fc550b4254
Jacob Barrieault <jacob.barrieault@gmail.com>
2025-04-06 14:33:50 -0400

    ASTParser#parse_array

--- json-parser-eager/ast_parser.rb
+++ json-parser-eager/ast_parser.rb
@@ -33,6 +33,16 @@ class StringNode < Node
   end
 end
 
+class ArrayNode < Node
+  def initialize(elements)
+    @elements = elements
+  end
+
+  def to_ruby
+    @elements.map(&:to_ruby)
+  end
+end
+
 class ASTParser
   def initialize(tokens:)
     @tokens = tokens
@@ -62,7 +72,18 @@ class ASTParser
   end
 
   def parse_array
+    expect(value: Lexer::LBRACKET)
+    elements = []
 
+    until current_token.nil? || current_token.value == Lexer::RBRACKET
+      val = parse
+      elements << val
+      expect(type: :SYMBOL, value: Lexer::COMMA) if current_token.value != Lexer::RBRACKET
+    end
+
+    expect(value: Lexer::RBRACKET)
+
+    ArrayNode.new(elements)
   end
 
   def parse_value
@@ -92,6 +113,18 @@ class ASTParser
     @position += 1
     @current_token = tokens[position]
   end
+
+  def expect(type: nil, value: nil)
+    if value && current_token.value != value
+      raise ParseError, "Expected token value #{value} at position #{position}, got: #{current_token.value}"
+    end
+
+    if type && current_token.type != type
+      raise ParseError, "Expected token type #{type} at position #{position}, got: #{current_token.type}"
+    end
+
+    advance
+  end
 end
 
 class ASTParserTest < Minitest::Test
@@ -113,6 +146,15 @@ class ASTParserTest < Minitest::Test
     assert_equal(3.14, node.to_ruby)
   end
 
+  def test_parse_with_scientific_float
+    token = [Token.new(type: :FLOAT, value: "6.8e4")]
+    parser = ASTParser.new(tokens: token)
+    node = parser.parse
+
+    assert_instance_of FloatNode, node
+    assert_equal(68000.0, node.to_ruby)
+  end
+
   def test_parse_with_string
     tokens = [Token.new(type: :STRING, value: '"According to all known laws of aviation, there is no way a bee should be able to fly."')]
     parser = ASTParser.new(tokens:)
@@ -124,4 +166,41 @@ class ASTParserTest < Minitest::Test
       node.to_ruby
     )
   end
+
+  def test_parse_with_empty_array
+    tokens = [[:SYMBOL, "["], [:SYMBOL, "]"]].map do |(type, value)|
+      Token.new(type:, value:)
+    end
+
+    parser = ASTParser.new(tokens:)
+    node = parser.parse
+
+    assert_instance_of(ArrayNode, node)
+    assert_equal([], node.to_ruby)
+  end
+
+  def test_parse_with_single_value_array
+    tokens = [[:SYMBOL, "["], [:INTEGER, 1], [:SYMBOL, "]"]].map do |(type, value)|
+      Token.new(type:, value:)
+    end
+
+    parser = ASTParser.new(tokens:)
+    node = parser.parse
+
+    assert_instance_of(ArrayNode, node)
+    assert_equal([1], node.to_ruby)
+  end
+
+  def test_parse_with_heterogeneous_array
+    tokens = [[:SYMBOL, "["], [:INTEGER, "1"], [:SYMBOL, ","], [:STRING, "\"b\""], [:SYMBOL, ","], [:FLOAT, "3.14"], [:SYMBOL, "]"]].map do |(type, value)|
+      Token.new(type:, value:)
+    end
+
+    parser = ASTParser.new(tokens:)
+    node = parser.parse
+
+    assert_instance_of(ArrayNode, node)
+    assert_equal([1, "b", 3.14], node.to_ruby)
+  end
+
 end
