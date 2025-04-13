5a9440d2a37c4e2ef94d54936b47b685788317d4
Jacob Barrieault <jacob.barrieault@gmail.com>
2025-04-13 16:45:41 -0400

    I discovered my implementations of Lexer#consume_string_literal_token do not handle escaped quotes

--- json-parser-streaming/lexer.rb
+++ json-parser-streaming/lexer.rb
@@ -18,6 +18,8 @@ class Token
 end
 
 class Lexer
+  class Wat < StandardError; end
+
   SPACE = " "
   NEWLINE = "\n"
   CARRIAGE_RETURN = "\r"
@@ -47,6 +49,8 @@ class Lexer
     COMMA
   ]
 
+  DOUBLE_QUOTE = "\""
+
   def initialize(source)
     @buffer = []
     @source = source
@@ -59,10 +63,33 @@ class Lexer
   def next_token
     eat_whitespace
 
-    char = getc
+    char = peek
     return if char.nil?
 
     return Token.new(type: :SYMBOL, value: char) if SYMBOLS.include? char
+
+    if char == DOUBLE_QUOTE
+      consume_string_literal_token
+    else
+      # consume_value_token
+    end
+  end
+
+  def consume_string_literal_token
+    if getc != DOUBLE_QUOTE
+      raise Wat, "\#consume_string_literal_token called, but the next char is not an opening quotation mark"
+    end
+
+    value = "" << DOUBLE_QUOTE
+
+    until (char = getc) == DOUBLE_QUOTE
+      raise Wat, "unclosed string literal '#{value}'" if char.nil?
+      value << char
+    end
+
+    value << DOUBLE_QUOTE
+
+    return Token.new(type: :STRING, value:)
   end
 
   def eat_whitespace
@@ -97,5 +124,19 @@ class LexerTest < Minitest::Test
     lexer = Lexer.new(source)
 
     assert_equal([:SYMBOL, "{"], lexer.next_token.to_a)
+  end
+
+  def test_string_literal_token
+    source = StringIO.new("\"hello\"")
+    lexer = Lexer.new(source)
+
+    assert_equal([:STRING, "\"hello\""], lexer.next_token.to_a)
+  end
+
+  def test_string_literal_token_with_escaped_double_quote
+    source = StringIO.new('"\"hello\""')
+    lexer = Lexer.new(source)
+
+    assert_equal([:STRING, '"\"hello\""'], lexer.next_token.to_a)
   end
 end
