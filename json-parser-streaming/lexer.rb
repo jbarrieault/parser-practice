9cbfc9f455d7e36dbb865e1eb9491eba9d50f4f2
Jacob Barrieault <jacob.barrieault@gmail.com>
2025-04-13 16:12:09 -0400

    Streaming lexer takes an IO and can eat whitespace

--- json-parser-streaming/lexer.rb
+++ json-parser-streaming/lexer.rb
@@ -1,16 +1,53 @@
 require "minitest/autorun"
 
 class Lexer
-  def initialize(reader)
-    @buffer = ""
-    @reader = reader # probably a StringIO?
+  SPACE = " "
+  NEWLINE = "\n"
+  CARRIAGE_RETURN = "\r"
+  TAB = "\t"
+
+  WHITESPACE_CHARACTERS = [
+    SPACE,
+    NEWLINE,
+    CARRIAGE_RETURN,
+    TAB
+  ]
+
+  def initialize(source)
+    @buffer = []
+    @source = source
   end
+
+  attr_reader :buffer, :source
 
   def next_token
+    eat_whitespace
   end
 
-  attr_reader :buffer, :reader
+  def eat_whitespace
+    while WHITESPACE_CHARACTERS.include?(peek)
+      getc
+    end
+  end
+
+  def getc
+    return @buffer.shift unless @buffer.empty?
+
+    source.getc
+  end
+
+  def peek
+    @buffer << source.getc if @buffer.empty?
+    @buffer[0]
+  end
 end
 
 class LexerTest < Minitest::Test
+  def test_eat_whitespace
+    source = StringIO.new("     a")
+    lexer = Lexer.new(source)
+    lexer.send(:eat_whitespace)
+
+    assert_equal("a", lexer.getc)
+  end
 end
