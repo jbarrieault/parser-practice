f963555c94b5f9846278c7665d5a47cd28549364
Jacob Barrieault <jacob.barrieault@gmail.com>
2025-04-20 22:00:50 -0400

    Parse string, but probably wrong

--- json-parser-streaming/parser.rb
+++ json-parser-streaming/parser.rb
@@ -40,8 +40,11 @@ class Event
 end
 
 class Parser
+  class ParseError < StandardError; end
+
   INTEGER_EVENT = "event:integer"
   FLOAT_EVENT = "event:float"
+  STRING_EVENT = "event:string"
 
   def initialize(lexer:, emitter:)
     @lexer = lexer
@@ -52,9 +55,10 @@ class Parser
   attr_reader :lexer, :current_token, :emitter
 
   def parse
-    return if advance.nil?
+    until advance.nil?
 
-    parse_value
+      parse_value
+    end
   end
 
   def parse_value
@@ -66,7 +70,8 @@ class Parser
       value = current_token.value.to_f
       emit(Event.new(type: FLOAT_EVENT, value:))
     when :STRING
-      # parse_string
+      value = current_token.value[1..-2]
+      emit(Event.new(type: STRING_EVENT, value:))
     end
   end
 
@@ -92,5 +97,19 @@ class ParserTest < Minitest::Test
 
     assert_equal(1, observer.events.size)
     assert_equal([Parser::INTEGER_EVENT, 42], event.to_a)
+  end
+
+  def test_parse_with_string
+    source = StringIO.new("\"Hello\"")
+    lexer = Lexer.new(source)
+    observer = MemoryObserver.new
+    emitter = Emitter.new(observers: [observer])
+    parser = Parser.new(lexer:, emitter:)
+
+    parser.parse
+    event = observer.events.last
+
+    assert_equal(1, observer.events.size)
+    assert_equal([Parser::STRING_EVENT, "Hello"], event.to_a)
   end
 end
