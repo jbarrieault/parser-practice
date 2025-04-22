b78137a0cc13de8825193e5a2b5d7da33ba1c109
Jacob Barrieault <jacob.barrieault@gmail.com>
2025-04-21 22:03:00 -0400

    Begin implenting array parsing in the SAX-like paradigm by transitioning the Parser to state-machine like approach

--- json-parser-streaming/parser.rb
+++ json-parser-streaming/parser.rb
@@ -45,20 +45,54 @@ class Parser
   INTEGER_EVENT = "event:integer"
   FLOAT_EVENT = "event:float"
   STRING_EVENT = "event:string"
+  ARRAY_START_EVENT = "event:array_start"
+  ARRAY_END_EVENT = "event:array_end"
 
   def initialize(lexer:, emitter:)
     @lexer = lexer
     @emitter = emitter
     @current_token = nil
+    @stack = []
+    @expecting = :value
   end
 
-  attr_reader :lexer, :current_token, :emitter
+  attr_reader :lexer, :current_token, :emitter, :stack, :expecting
 
   def parse
-    until advance.nil?
+    return if advance.nil?
 
+    if current_token.value == Lexer::LBRACKET
+      parse_array
+    elsif current_token.value == Lexer::RBRACE
+      # TODO
+      # parse_object
+    else
       parse_value
     end
+  end
+
+  # re-entrant API: only emits 1 event per call
+  def parse_next
+    advance
+
+    if current_token.nil?
+      # error if: @stack isn't empty (something is un-closed)
+      raise ParseError, "unexpected end of input: stack not empty" if stack.size > 0
+      return
+    end
+
+    if current_token.value == Lexer::LBRACKET
+
+      parse_array_start
+    end
+  end
+
+  def parse_array_start
+    expect!(:value)
+    stack.push(:array)
+    expecting = :value # :value_or_bracket
+
+    emit(Event.new(type: ARRAY_START_EVENT, value: current_token.value))
   end
 
   def parse_value
@@ -81,6 +115,12 @@ class Parser
 
   def emit(event)
     emitter.emit(event)
+  end
+
+  def expect!(type_being_parsed)
+    if type_being_parsed != expecting
+      raise ParseError, "Expecting to parse a #{expecting}, but parsing a #{type_being_parsed}"
+    end
   end
 end
 
@@ -111,5 +151,24 @@ class ParserTest < Minitest::Test
 
     assert_equal(1, observer.events.size)
     assert_equal([Parser::STRING_EVENT, "Hello"], event.to_a)
+  end
+
+  def test_parse_next_array_start
+    source = StringIO.new("[")
+    lexer = Lexer.new(source)
+    observer = MemoryObserver.new
+    emitter = Emitter.new(observers: [observer])
+    parser = Parser.new(lexer:, emitter:)
+
+    parser.parse_next
+    event = observer.events.last
+
+    # test what was emitted
+    assert_equal(1, observer.events.size)
+    assert_equal([Parser::ARRAY_START_EVENT, "["], event.to_a)
+
+    # test state transition
+    assert_equal([:array], parser.stack)
+    assert_equal(:value, parser.expecting)
   end
 end
