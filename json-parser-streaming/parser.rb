dd367a77c4f203fa24a6f1da92381243344f7668
Jacob Barrieault <jacob.barrieault@gmail.com>
2025-04-25 11:13:28 -0400

    parse objects, parse adjacent arrays & objsects

--- json-parser-streaming/parser.rb
+++ json-parser-streaming/parser.rb
@@ -51,6 +51,7 @@ class Parser
   ARRAY_END_EVENT = "event:array_end"
   OBJECT_START_EVENT = "event:object_start"
   OBJECT_END_EVENT = "event:object_end"
+  OBJECT_KEY_EVENT = "event:object_key"
 
   def initialize(lexer:, emitter:)
     @lexer = lexer
@@ -62,15 +63,9 @@ class Parser
   attr_reader :lexer, :current_token, :emitter, :stack
 
   def parse
-    return if advance.nil?
-
-    if current_token.value == Lexer::LBRACKET
-      parse_array
-    elsif current_token.value == Lexer::RBRACE
-      # TODO
-      # parse_object
-    else
-      parse_value
+    loop do
+      parse_next
+      break if current_token.nil?
     end
   end
 
@@ -82,6 +77,7 @@ class Parser
     when nil
       # An array or object is un-closed)
       raise ParseError, "unexpected end of input: stack not empty" if stack.size > 0
+      return nil
     when Lexer::LBRACKET
       parse_array_start
     when Lexer::RBRACKET
@@ -96,8 +92,14 @@ class Parser
       # in order to preserve the behavior of 1 public call to #next_token emitting an event.
       parse_next
     else
-      parse_value
+      if current_token.type == :STRING
+        parse_string
+      else
+        parse_value
+      end
     end
+
+    true
   end
 
   def parse_array_start
@@ -109,6 +111,11 @@ class Parser
   def parse_array_end
     must_expect!(:array_end)
     stack.pop
+
+    # this represents the end of a value a parent array or object was expecting
+    state[:expecting] = [:comma, :object_end] if state[:type] == :object
+    state[:expecting] = [:comma, :array_end] if state[:type] == :array
+
     emit(Event.new(type: ARRAY_END_EVENT, value: current_token.value))
   end
 
@@ -121,31 +128,73 @@ class Parser
   def parse_object_end
     must_expect!(:object_end)
     stack.pop
+
+    # this represents the end of a value a parent array or object was expecting
+    state[:expecting] = [:comma, :object_end] if state[:type] == :object
+    state[:expecting] = [:comma, :array_end] if state[:type] == :array
+
     emit(Event.new(type: OBJECT_END_EVENT, value: current_token.value))
+  end
+
+  def parse_string
+    if state[:expecting]&.include?(:key)
+      parse_key
+    else
+      parse_string_value
+    end
+  end
+
+  def parse_string_value
+    must_expect!(:value)
+
+    value = current_token.value[1..-2]
+
+    state[:expecting] = [:comma, :array_end] if state[:type] == :array
+    state[:expecting] = [:comma, :object_end] if state[:type] == :object
+
+    emit(Event.new(type: STRING_EVENT, value:))
+  end
+
+  def parse_key
+    must_expect!(:key)
+
+    if current_token.type != :STRING
+      raise ParseError, "Unexpected token '#{current_token.value}', expected string for object key"
+    end
+
+    value = current_token.value[1..-2]
+
+    advance
+    if current_token.to_a != [:SYMBOL, ":"]
+      raise ParseError, "Unexpected token '#{current_token.value}', expected ':' for object key"
+    end
+
+    state[:expecting] = [:value]
+
+    emit(Event.new(type: OBJECT_KEY_EVENT, value:))
   end
 
   def parse_value
     must_expect!(:value)
 
-    case current_token.type
+    event = case current_token.type
     when :INTEGER
       value = current_token.value.to_i
-      emit(Event.new(type: INTEGER_EVENT, value:))
+      Event.new(type: INTEGER_EVENT, value:)
     when :FLOAT
       value = current_token.value.to_f
-      emit(Event.new(type: FLOAT_EVENT, value:))
-    when :STRING
-      value = current_token.value[1..-2]
-      emit(Event.new(type: STRING_EVENT, value:))
+      Event.new(type: FLOAT_EVENT, value:)
     when :NULL
-      emit(Event.new(type: NULL_EVENT, value: nil))
+      Event.new(type: NULL_EVENT, value: nil)
     when :BOOL
       value = current_token.value == "true"
-      emit(Event.new(type: BOOL_EVENT, value:))
+      Event.new(type: BOOL_EVENT, value:)
     end
 
     state[:expecting] = [:comma, :array_end] if state[:type] == :array
-    # state[:expecting] = [:comma, :end_object] if state[:type] == :object
+    state[:expecting] = [:comma, :object_end] if state[:type] == :object
+
+    emit(event)
   end
 
   def parse_comma
@@ -153,8 +202,8 @@ class Parser
 
     if state[:type] == :array
       state[:expecting] = [:value]
-    # elsif state[:type] == :object
-    #   state[:expecting] = [:key, :value]
+    elsif state[:type] == :object
+      state[:expecting] = [:key]
     end
   end
 
@@ -176,7 +225,7 @@ class Parser
     return if expecting.nil?
 
     unless expecting.include?(type)
-      raise ParseError, "Unexpected #{type} '#{current_token.value}', expecting any of: #{expecting.join(', ')}"
+      raise ParseError, "Unexpected token '#{current_token.value}' (type #{type}), expecting any of: #{expecting.join(', ')}"
     end
   end
 end
@@ -197,7 +246,7 @@ class ParserTest < Minitest::Test
     assert_equal([Parser::INTEGER_EVENT, 42], event.to_a)
   end
 
-  def test_parse_value_with_string
+  def test_parse_string_value
     source = StringIO.new("\"Hello\"")
     lexer = Lexer.new(source)
     observer = MemoryObserver.new
@@ -205,7 +254,7 @@ class ParserTest < Minitest::Test
     parser = Parser.new(lexer:, emitter:)
 
     parser.advance
-    parser.parse_value
+    parser.parse_string_value
     event = observer.events.last
 
     assert_equal(1, observer.events.size)
@@ -269,7 +318,18 @@ class ParserTest < Minitest::Test
   end
 
   def test_parse_next_object
-    source = StringIO.new("{}")
+    source = StringIO.new(
+      <<~JSON
+        {
+          "hello": "world",
+          "hobbies": ["programming", "pickleball", "bicycling", "music"],
+          "detail": {
+            "ts": 123456,
+            "args": [1, "16", true]
+          }
+        }
+      JSON
+    )
     lexer = Lexer.new(source)
     observer = MemoryObserver.new
     emitter = Emitter.new(observers: [observer])
@@ -277,6 +337,66 @@ class ParserTest < Minitest::Test
 
     parser.parse_next
     assert_equal([Parser::OBJECT_START_EVENT, "{"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::OBJECT_KEY_EVENT, "hello"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::STRING_EVENT, "world"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::OBJECT_KEY_EVENT, "hobbies"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::ARRAY_START_EVENT, "["], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::STRING_EVENT, "programming"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::STRING_EVENT, "pickleball"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::STRING_EVENT, "bicycling"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::STRING_EVENT, "music"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::ARRAY_END_EVENT, "]"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::OBJECT_KEY_EVENT, "detail"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::OBJECT_START_EVENT, "{"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::OBJECT_KEY_EVENT, "ts"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::INTEGER_EVENT, 123456], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::OBJECT_KEY_EVENT, "args"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::ARRAY_START_EVENT, "["], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::INTEGER_EVENT, 1], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::STRING_EVENT, "16"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::BOOL_EVENT, true], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::ARRAY_END_EVENT, "]"], observer.events.last.to_a)
+
+    parser.parse_next
+    assert_equal([Parser::OBJECT_END_EVENT, "}"], observer.events.last.to_a)
 
     parser.parse_next
     assert_equal([Parser::OBJECT_END_EVENT, "}"], observer.events.last.to_a)
