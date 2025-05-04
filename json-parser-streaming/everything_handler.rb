24d93426f1ad149609fc7a1bc9914011824a84ba
Jacob Barrieault <jacob.barrieault@gmail.com>
2025-05-04 08:14:05 -0400

    Parse object that ends with an array value
    But the local maximum has beebn reached for this approach—after closing the array value we have no reference to the parent object and cannot continue handling. I believe each stack entry needs a key that holds its value object

--- json-parser-streaming/everything_handler.rb
+++ json-parser-streaming/everything_handler.rb
@@ -9,6 +9,7 @@ require_relative "parser"
 class EverythingHandler
   def initialize
     @value = nil
+    @cursor = nil
     @current_event = nil
     @stack = [{ type: :top, buffer: nil } ]
   end
@@ -19,6 +20,16 @@ class EverythingHandler
     @current_event = event
     case event.type
     when Parser::ARRAY_START_EVENT
+      # left off: this is the start of a value that needs to be either:
+      # 1. the top-level value of the doc
+      # 2. shoveled into a currently open array
+      # 3. added to currently object object
+      # so maybe this class should be scrapped to build an actual AST
+      # then handle_array_start would be more like:
+      # node =  ArrayNode.new(event)
+      # @current_node.add_child(node)
+      # @current_node = node
+      # and closing arrays & objects would re-set @current_node to @current_node.parent
       handle_array_start
     when Parser::ARRAY_END_EVENT
       handle_array_end
@@ -34,8 +45,24 @@ class EverythingHandler
   end
 
   def handle_array_start
+    if @value.nil?
+      @value = @cursor = []
+    else
+      next_cursor = []
+      if stack.last[:type] == :array
+        puts "BACON current_event: #{current_event.value}"
+        puts "BACON stack.last: #{stack.last}"
+        @cursor << next_cursor
+        @cursor = next_cursor
+      elsif stack.last[:type] == :object
+        @cursor[stack.last[:buffer]] = next_cursor
+        @cursor = next_cursor
+      else
+        raise "wat"
+      end
+    end
+
     @stack << { type: :array }
-    @value = []
   end
 
   def handle_array_end
@@ -43,13 +70,28 @@ class EverythingHandler
       raise "unexpected event value '#{current_event.value}', expected ']'"
     end
 
+    # we're screwed—we don't know how to move the cursor up the the current arrays's parent
+    # that tells me that each @stack entry needs a cursor?
     stack.pop
-    :noop
   end
 
   def handle_object_start
+    if @value.nil?
+      @value = @cursor = {}
+    else
+      next_cursor = {}
+      if stack.last[:type] == :array
+        @cursor << next_cursor
+        @cursor = next_cursor
+      elsif stack.last[:type] == :object
+        @cursor[stack.last[:buffer]] = next_cursor
+        @cursor = next_cursor
+      else
+        raise "wat"
+      end
+    end
+
     stack << { type: :object }
-    @value = {}
   end
 
   def handle_object_key
@@ -65,19 +107,19 @@ class EverythingHandler
       raise "unexpected event value '#{current_event.value}', expected '}'"
     end
 
+    # we're screwed—we don't know how to move the cursor up the the current object's parent
     stack.pop
-    :noop
   end
 
   def handle_value(value)
     state = stack.last
     if state[:type] == :array
-      @value << value
+      @cursor << value
     elsif state[:type] == :object
       key = state[:buffer]
-      @value[key] = value
+      @cursor[key] = value
     else
-      @value = value
+      @value = @cursor = value
     end
   end
 
@@ -87,8 +129,44 @@ class EverythingHandler
 end
 
 class EverythingHandlerTest < Minitest::Test
-  def test_handle_integer
-    source = StringIO.new("1")
+  # def test_handle_integer
+  #   source = StringIO.new("1")
+  #   lexer = Lexer.new(source)
+  #   handler = EverythingHandler.new
+  #   emitter = Emitter.new(observers: [handler])
+  #   parser = Parser.new(lexer:, emitter:)
+
+  #   parser.parse
+
+  #   assert_equal(1, handler.to_ruby)
+  # end
+
+  # def test_handle_array
+  #   source = StringIO.new("[1,2,3]")
+  #   lexer = Lexer.new(source)
+  #   handler = EverythingHandler.new
+  #   emitter = Emitter.new(observers: [handler])
+  #   parser = Parser.new(lexer:, emitter:)
+
+  #   parser.parse
+
+  #   assert_equal([1,2,3], handler.to_ruby)
+  # end
+
+  # def test_handle_object
+  #   source = StringIO.new('{ "hello": "world" }')
+  #   lexer = Lexer.new(source)
+  #   handler = EverythingHandler.new
+  #   emitter = Emitter.new(observers: [handler])
+  #   parser = Parser.new(lexer:, emitter:)
+
+  #   parser.parse
+
+  #   assert_equal({ "hello" => "world" }, handler.to_ruby)
+  # end
+
+  def test_nested_structures
+    source = StringIO.new('{ "hello": "world", "numbers": [1,2,3] }')
     lexer = Lexer.new(source)
     handler = EverythingHandler.new
     emitter = Emitter.new(observers: [handler])
@@ -96,31 +174,7 @@ class EverythingHandlerTest < Minitest::Test
 
     parser.parse
 
-    assert_equal(1, handler.to_ruby)
-  end
-
-  def test_handle_array
-    source = StringIO.new("[1,2,3]")
-    lexer = Lexer.new(source)
-    handler = EverythingHandler.new
-    emitter = Emitter.new(observers: [handler])
-    parser = Parser.new(lexer:, emitter:)
-
-    parser.parse
-
-    assert_equal([1,2,3], handler.to_ruby)
-  end
-
-  def test_handle_object
-    source = StringIO.new('{ "hello": "world" }')
-    lexer = Lexer.new(source)
-    handler = EverythingHandler.new
-    emitter = Emitter.new(observers: [handler])
-    parser = Parser.new(lexer:, emitter:)
-
-    parser.parse
-
-    assert_equal({ "hello" => "world" }, handler.to_ruby)
+    assert_equal({ "hello" => "world", "numbers" => [1,2,3] }, handler.to_ruby)
   end
 end
 
